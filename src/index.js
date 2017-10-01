bc86231339af540a331b176d5185eb5402453880
robflop <robflop98@gmail.com>
2017-10-01 17:40:53 +0200

    buggy multi-filtering

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = new Vue({
 		isLoaded: false,
 		matchInfoInput: '',
 		chatLogsInput: '',
+		savedChatLogs: '',
 		parsedMatchInfo: '',
 		parsedChatLogs: '',
 		selectedType: 'All',
@@ -13,20 +14,22 @@ const app = new Vue({
 		involvedFilter: false,
 		days: 0,
 		nights: 0,
-		savedChatLogs: ''
+		isFiltered: false
 	},
 	mounted() {
 		//
 	},
 	methods: {
 		loadMatch() {
-			this.parsedMatchInfo = JSON.parse(this.matchInfoInput);
-			this.parsedChatLogs = JSON.parse(this.chatLogsInput).filter(line => line);
+			// this.parsedMatchInfo = JSON.parse(this.matchInfoInput);
+			this.parsedMatchInfo = require('../abc').info; // todo temporary
+			// this.parsedChatLogs = JSON.parse(this.chatLogsInput).filter(line => line).map(line => line.trim());
+			this.parsedChatLogs = require('../abc.js').logs.filter(line => line).map(line => line.trim()); // todo temporary
 			this.savedChatLogs = this.parsedChatLogs;
 			this.days = this.savedChatLogs.filter(line => /\(Day\) Day \d+/.test(line)).length;
 			this.nights = this.savedChatLogs.filter(line => /\(Day\) Night \d+/.test(line)).length;
 
-			return this.isLoaded = !this.isLoaded;
+			return this.isLoaded = true;
 		},
 
 		unloadMatch() {
@@ -35,11 +38,17 @@ const app = new Vue({
 			this.days = 0; this.nights = 0;
 			this.savedChatLogs = '';
 
-			return this.isLoaded = !this.isLoaded;
+			return this.isLoaded = false;
 		},
 
 		checkType(log) {
-			//
+			const types = {
+				Announcement: 'announcement'
+			};
+
+			// console.log(log);
+
+			// return 'alive';
 		},
 
 		filter() {
@@ -48,32 +57,35 @@ const app = new Vue({
 					if (this.selectedType === 'Whisper') {
 						// whispers are special because they don't follow the format of other types
 						const whisperRegex = /From [\w\s]+ \[\d+] \([\w\s]+\):/;
-						return this.parsedChatLogs = this.savedChatLogs.filter(line => {
+						return this.parsedChatLogs = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs'].filter(line => {
 							return whisperRegex.test(line) || line.startsWith('(Day)') || line.startsWith('(Win)');
 						});
 					}
 
-					this.parsedChatLogs = this.savedChatLogs.filter(line => {
+					this.parsedChatLogs = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs'].filter(line => {
 						return line.startsWith(`(${this.selectedType}`) || line.startsWith('(Day)') || line.startsWith('(Win)');
 					});
 				}
 
 				if (this.selectedPlayer !== 'All') {
 					const playerRegex = new RegExp(`${this.selectedPlayer} \\[\\d+\\]`);
-					this.parsedChatLogs = this.savedChatLogs.filter(line => {
+					this.parsedChatLogs = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs'].filter(line => {
 						return playerRegex.test(line) || line.startsWith('(Day)') || line.startsWith('(Win)');
 					});
 				}
 
 				if (this.selectedDay !== 'All') {
-					const start = this.savedChatLogs.indexOf(`(Day) ${this.selectedDay}`);
-					let end = this.savedChatLogs.indexOf(`(Day) Night ${parseInt(this.selectedDay.replace('Day', ''))}`);
+					const start = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs']
+						.indexOf(`(Day) ${this.selectedDay}`);
+					let end = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs']
+						.indexOf(`(Day) Night ${parseInt(this.selectedDay.replace('Day', ''))}`);
+
 					const iterated = [];
 
 					if (parseInt(this.selectedDay.replace('Day', '')) === this.days) end = this.savedChatLogs.length;
 					// for last day since there isn't a night following it to detect
 
-					this.parsedChatLogs = this.savedChatLogs.filter((line, index) => {
+					this.parsedChatLogs = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs'].filter((line, index) => {
 						if (iterated.includes(index)) { return false; }
 						if (index >= start && index <= end) { iterated.push(index); return true; }
 						else { return false; }
@@ -82,11 +94,14 @@ const app = new Vue({
 				}
 
 				if (this.selectedNight !== 'All') {
-					const start = this.savedChatLogs.indexOf(`(Day) ${this.selectedNight}`);
-					const end = this.savedChatLogs.indexOf(`(Day) Day ${parseInt(this.selectedNight.replace('Night', '')) + 1}`);
+					const start = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs']
+						.indexOf(`(Day) ${this.selectedNight}`);
+					const end = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs']
+						.indexOf(`(Day) Day ${parseInt(this.selectedNight.replace('Night', '')) + 1}`);
+
 					const iterated = [];
 
-					this.parsedChatLogs = this.savedChatLogs.filter((line, index) => {
+					this.parsedChatLogs = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs'].filter((line, index) => {
 						if (iterated.includes(index)) { return false; }
 						if (index >= start && index <= end) { iterated.push(index); return true; }
 						else { return false; }
@@ -97,13 +112,16 @@ const app = new Vue({
 				if (this.involvedFilter) {
 					const playerRegex = new RegExp(`${this.selectedPlayer} \\[\\d+\\]`);
 
-					this.parsedChatLogs = this.savedChatLogs.filter(line => {
+					this.parsedChatLogs = this[this.isFiltered ? 'parsedChatLogs' : 'savedChatLogs'].filter(line => {
 						//
 					});
 				}
+
+				this.isFiltered = true;
 			}
 			else {
 				this.parsedChatLogs = this.savedChatLogs;
+				this.isFiltered = false;
 			}
 		}
 	},
