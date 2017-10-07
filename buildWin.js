93eb7461149aaacbbd8e811c18cc065835330c32
Robin B <robflop98@web.de>
2017-10-07 15:18:35 +0200

    Update dependencies, version, build scripts

---
+++ buildWin.js
@@ -0,0 +1,24 @@
+const packager = require('electron-packager');
+
+console.log('Now building... (This might take a while)');
+
+const buildOptions = {
+	dir: './',
+	appCopyright: 'Copyright (C) 2017 robflop',
+	platform: 'win32',
+	arch: process.argv[2] || 'ia32',
+	asar: true,
+	icon: './src/icon.ico',
+	ignore: /(buildWin|makeInstallers|abc|vscode|eslintrc|gitattributes|gitignore|htmlhintrc|travis)/,
+	out: 'dist/',
+	overwrite: true,
+	prune: true,
+	win32metadata: {
+		CompanyName: 'robflop',
+		ProductName: 'ToL-Mod-Utility',
+		FileDescription: 'Moderation utility for Throne of Lies player reports.',
+		OriginalFilename: 'ToL-Mod-Utility.exe'
+	}
+};
+
+packager(buildOptions).then(appPaths => console.log('Successfully built into the following folder:', appPaths.join('\n')));
