3dd29d97b3f389c7884aa28c2b3b4fd991f51e18
Robin B <robflop98@web.de>
2017-11-19 02:28:09 +0100

    Adjust client size & back to minified vue for next release

--- src/client.js
+++ src/client.js
@@ -3,15 +3,17 @@ const { productName, version, description } = require('../package.json');
 app.setAppUserModelId('com.robflop.tol-mod-utility');
 
 app.on('ready', () => {
-	const mainWindow = new BrowserWindow({ minWidth: 1080, minHeight: 720, webPreferences: { devTools: false }, center: true, show: false });
+	// require('vue-devtools').install();
+
+	const mainWindow = new BrowserWindow({ minWidth: 1080, minHeight: 820, webPreferences: { devTools: false }, center: true, show: false });
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 	mainWindow.on('ready-to-show', () => {
-		// mainWindow.webContents.openDevTools();
 		mainWindow.setMenu(null);
-		mainWindow.setSize(1080, 720);
+		mainWindow.setSize(1080, 820);
 		mainWindow.setTitle(`${productName} ${version} - ${description}`);
 		mainWindow.show();
 	});
+	// mainWindow.webContents.openDevTools();
 });
 
 app.on('window-all-closed', () => app.quit());
