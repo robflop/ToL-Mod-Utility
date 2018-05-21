050d45b65b14e60fbd4fc8ac595e6fe27a5cbe24
Robin B <robflop98@web.de>
2018-05-21 21:16:52 +0200

    Adjust Window height

--- src/client.js
+++ src/client.js
@@ -5,11 +5,11 @@ app.setAppUserModelId('com.robflop.tol-mod-utility');
 app.on('ready', () => {
 	// require('vue-devtools').install();
 
-	const mainWindow = new BrowserWindow({ minWidth: 1420, minHeight: 820, webPreferences: { devTools: true }, center: true, show: false });
+	const mainWindow = new BrowserWindow({ minWidth: 1420, minHeight: 840, webPreferences: { devTools: false }, center: true, show: false });
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 	mainWindow.on('ready-to-show', () => {
 		mainWindow.setMenu(null);
-		mainWindow.setSize(1420, 820);
+		mainWindow.setSize(1420, 840);
 		mainWindow.setTitle(`${productName} ${version} - ${description}`);
 		mainWindow.show();
 	});
