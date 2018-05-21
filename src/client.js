661d95fdbacd8d80efbc36f67ce5840a4e113694
Robin B <robflop98@web.de>
2018-05-21 20:19:26 +0200

    Player Journals (beginning)
    b

--- src/client.js
+++ src/client.js
@@ -5,11 +5,11 @@ app.setAppUserModelId('com.robflop.tol-mod-utility');
 app.on('ready', () => {
 	// require('vue-devtools').install();
 
-	const mainWindow = new BrowserWindow({ minWidth: 1080, minHeight: 820, webPreferences: { devTools: false }, center: true, show: false });
+	const mainWindow = new BrowserWindow({ minWidth: 1420, minHeight: 820, webPreferences: { devTools: true }, center: true, show: false });
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 	mainWindow.on('ready-to-show', () => {
 		mainWindow.setMenu(null);
-		mainWindow.setSize(1080, 820);
+		mainWindow.setSize(1420, 820);
 		mainWindow.setTitle(`${productName} ${version} - ${description}`);
 		mainWindow.show();
 	});
