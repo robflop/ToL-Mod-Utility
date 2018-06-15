b99197ba492a193528448728b08d3b2a2291adcc
Robin B <robflop98@web.de>
2018-06-16 00:35:14 +0200

    Measure prep work

--- src/client.js
+++ src/client.js
@@ -5,8 +5,14 @@ app.setAppUserModelId('com.robflop.tol-mod-utility');
 app.on('ready', () => {
 	// require('vue-devtools').install();
 
-	const mainWindow = new BrowserWindow({ minWidth: 1420, minHeight: 840, webPreferences: { devTools: false }, center: true, show: false });
-	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	const mainWindow = new BrowserWindow({
+		minWidth: 1420,
+		minHeight: 840,
+		webPreferences: { devTools: false },
+		center: true,
+		show: false
+	});
+
 	mainWindow.loadURL(`file://${__dirname}/html/index.html`);
 	mainWindow.on('ready-to-show', () => {
 		mainWindow.setMenu(null);
@@ -14,6 +20,8 @@ app.on('ready', () => {
 		mainWindow.setTitle(`${productName} ${version} - ${description}`);
 		mainWindow.show();
 	});
+	mainWindow.on('closed', () => app.quit());
+
 	// mainWindow.webContents.openDevTools();
 });
 
